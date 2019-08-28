75cce0f080e90c120b4e6a616d88498ca00b5dbf
davalapar <davalapar@gmail.com>
2019-08-28 08:58:18 +0800

    yeh

--- index.js
+++ index.js
@@ -7,6 +7,9 @@
  */
 
 /**
+ * - Database provides Transactions
+ * - Transactions provides Tables, Queries, and Items
+ *
  * Table(tableId):
  * - insert(itemId, itemData)
  * - update(itemId, itemData)
@@ -15,8 +18,38 @@
  * - has(itemId)
  *
  * Transaction(tablesSource):
- * - table(tableId)
- * - item(tableId, itemId)
+ * - table(tableId) -> Table
+ * - query(tableId) -> Query
+ * - item(tableId, itemId) -> Item
+ *
+ * Query(itemsSource):
+ * - offset(value)
+ * - limit(value)
+ * - ascend(field)
+ * - descend(field)
+ * - deprecated, merged with ascend: ascendHaversine (field, lat, long)
+ * - deprecated, merged with descend: descendHaversine (field, lat, long)
+ * - sortBy(sortFunction)
+ * - gt(field, value)
+ * - gte(field, value)
+ * - lt(field, value)
+ * - lte(field, value)
+ * - eq(field, value)
+ * - neq(field, value)
+ * - eqAny(field, values)
+ * - neqAll(field, values)
+ * - includes(field, value)
+ * - includesAny(field, values)
+ * - includesAll(field, values)
+ * - includesNoneOfAny(field, values)
+ * - includesNoneOfAll(field, values)
+ * - reveal(fields)
+ * - conceal(fields)
+ * - filterBy(filterFunction)
+ * - results() -> Items
+ * - firstResult() -> Item
+ * - hasResults() -> Boolean
+ * - countResults() -> Number
  */
 
 function Table(tableId) {
